36146da71bc9d4450efb8ac207c5ecf6f22075c7
qiph7 <275775988@qq.com>
2016-12-08 21:12:38 +0800

    add the typedef.c file

---
+++ Typedef.c
@@ -0,0 +1,16 @@
+#ifndef __TYPEDEF_H__
+#define __TYPEDEF_H__
+
+// Definition of types
+typedef unsigned char           u8;
+typedef signed char             s8;
+typedef unsigned short          u16;
+typedef signed short            s16;
+typedef unsigned int            u32;
+typedef signed int              s32;
+typedef unsigned long long      u64;
+typedef signed long long        s64;
+typedef float                   f32;
+typedef double float            f64;
+
+#endif  // __TYPEDEF_H__
